e198bc2d07551c89163bf0867d9492bfc1b0d224
sentixzel <sentixzel@gmail.com>
2025-06-10 16:50:30 +0300

    Добавьте файлы проекта.

---
+++ CleverenceSoft_TestTask2/MyServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CleverenceSoft_TestTask2
+{
+    public static class MyServer
+    {
+        private static int count = 0;
+        private static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                //Console.WriteLine($"Reader {Thread.CurrentThread.ManagedThreadId}: Count = {_count}");
+                _lock.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                //Console.WriteLine($"Writer {Thread.CurrentThread.ManagedThreadId}: Added {value}, Count = {_count}");
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
---
+++ CleverenceSoft_TestTask2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverenceSoft_TestTask2
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            // Запускаем несколько потоков для чтения и записи
+            Task[] tasks = new Task[10];
+
+            Random random = new Random();
+
+            int i = 0;
+            for (; i < 5; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        Console.WriteLine($"Reader {Thread.CurrentThread.ManagedThreadId}: Count = {MyServer.GetCount()}");
+                        Thread.Sleep(random.Next(100)); // Имитация некоторой работы
+                    }
+                });
+            }
+
+            for (; i < 10; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        MyServer.AddToCount(j);
+
+                        // Хотя, добавление к MyServer.count работает корректно и писатели работают последовательно, 
+                        // но вывод именно здесь, не в MyServer.AddToCount (для), не будет корректным
+                        // и за то время, после выхода из MyServer.AddToCount
+                        // до вывода, другой "писатель" успеет вызвать MyServer.AddToCount
+
+                        Console.WriteLine($"Writer {Thread.CurrentThread.ManagedThreadId}: Added {j}, Count = {MyServer.GetCount()}");
+                        Thread.Sleep(random.Next(1000)); // Имитация некоторой работы
+                    }
+                });
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine($"Final Count: {MyServer.GetCount()}");
+        }
+    }
+}
